8548329a290ef2b9feb5ce13fb63fa5b2ed1c8f3
sepehr-amooei <sepehramooei@gmail.com>
2023-07-25 13:38:27 +0330

    index editing

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
-import "bootstrap/js/src/dropdown";
 import "font-awesome/css/font-awesome.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
