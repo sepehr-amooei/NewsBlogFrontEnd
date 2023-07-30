4e043bd21696b3b1eccf4145ebb2910c39f7e9e7
sepehr-amooei <sepehramooei@gmail.com>
2023-07-30 14:56:35 +0330

    make controlled input element

--- src/component/common/loginForm.jsx
+++ src/component/common/loginForm.jsx
@@ -1,25 +1,53 @@
 import React, { Component } from "react";
+import { Form } from "react-router-dom";
 
 class LoginForm extends Component {
+  state = {
+    account: { username: "", password: "" },
+  };
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
   render() {
+    const { account } = this.state;
     return (
       <div className="center">
         <h1>Login</h1>
-        <form>
+        <Form>
           <div className="mb-3">
             <label htmlFor="username" className="form-label">
               Username
             </label>
-            <input id="username" type="text" className="form-control" />
+            <input
+              value={account.username}
+              name="username"
+              onChange={this.handleChange}
+              autoFocus
+              id="username"
+              type="text"
+              className="form-control"
+            />
           </div>
           <div className="mb-3">
             <label htmlFor="password" className="form-label">
               Password
             </label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              value={account.password}
+              name="password"
+              onChange={this.handleChange}
+              id="password"
+              type="text"
+              className="form-control"
+            />
           </div>
-          <button className="btn btn-primary">Login</button>
-        </form>
+          <button type="submit" className="btn btn-primary">
+            Login
+          </button>
+        </Form>
       </div>
     );
   }
