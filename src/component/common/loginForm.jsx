2d07c6767fb1c80906b768482b602a1db93171c0
sepehr-amooei <sepehramooei@gmail.com>
2023-07-30 12:16:18 +0330

    Building Login Forn

--- src/component/common/loginForm.jsx
+++ src/component/common/loginForm.jsx
@@ -5,6 +5,21 @@ class LoginForm extends Component {
     return (
       <div className="center">
         <h1>Login</h1>
+        <form>
+          <div className="mb-3">
+            <label htmlFor="username" className="form-label">
+              Username
+            </label>
+            <input id="username" type="text" className="form-control" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input id="password" type="text" className="form-control" />
+          </div>
+          <button className="btn btn-primary">Login</button>
+        </form>
       </div>
     );
   }
