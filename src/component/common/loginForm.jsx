039d389aa34eac37be7207e55680c4b449f29349
sepehr-amooei <sepehramooei@gmail.com>
2023-07-31 12:25:51 +0330

    validation on change

--- src/component/common/loginForm.jsx
+++ src/component/common/loginForm.jsx
@@ -29,9 +29,23 @@ class LoginForm extends Component {
     return Object.keys(errors).length === 0 ? null : errors;
   };
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
+  };
+  validateProperty = ({ name, value }) => {
+    if (name === "username") {
+      if (value.trim() === "") return "Username is required!";
+    }
+
+    if (name === "password") {
+      if (value.trim() === "") return "Password is required!";
+    }
   };
 
   render() {
