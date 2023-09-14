271db6e643755beb64fac8d80c2fdfd708506fc5
taha <tahamiri02@gmail.com>
2023-09-14 16:05:45 +0330

    news model.

--- api/API/news/models.py
+++ api/API/news/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+CATEGORY_CHOICES = (
+    ("sport", "sport"),
+    
+)
+
+
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    category = CATEGORY_CHOICES
+    view = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    intro = models.TextField()
+    body = models.TextField()
+    image = models.ImageField(upload_to="images/%Y/%m/%d/")
+
+    class Meta:
+        verbose_name = "news"
+        verbose_name_plural = "news"
+
+    def __str__(self):
+        return self.title
+    
