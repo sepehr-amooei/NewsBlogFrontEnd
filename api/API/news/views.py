5f631e43223fa8f2c2291bff3e2ab5ae6e98a412
taha <tahamiri02@gmail.com>
2023-09-14 22:30:14 +0330

    add sort option to newsreview.

--- api/API/news/views.py
+++ api/API/news/views.py
@@ -11,7 +11,7 @@ class NewsReview(APIView, LimitOffsetPagination):
     '''
     list of news.
     '''
-    #permission_classes = [IsAuthenticated,]
+    permission_classes = [IsAuthenticated,]
     serializer_class = NewsSerializer
     
     def get(self, request):
