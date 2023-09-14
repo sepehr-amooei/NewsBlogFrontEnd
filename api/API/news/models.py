e863632c5573a26c0d6aa65b6b843e3246ff21c1
taha <tahamiri02@gmail.com>
2023-09-14 20:27:25 +0330

    add category field to news model.

--- api/API/news/models.py
+++ api/API/news/models.py
@@ -3,13 +3,18 @@ from django.db import models
 
 CATEGORY_CHOICES = (
     ("sport", "sport"),
-    
+    ("movie", "movie"),
+    ("celebrity", "celebrity"),
 )
 
 
 class News(models.Model):
     title = models.CharField(max_length=255)
-    category = CATEGORY_CHOICES
+    category = models.CharField(
+        max_length = 20,
+        choices = CATEGORY_CHOICES,
+        default = 'sport'
+        )
     view = models.IntegerField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
