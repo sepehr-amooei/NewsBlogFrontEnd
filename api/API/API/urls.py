96f31e5fd84e482fd0bec8a4c7ec8952e660b73e
taha <tahamiri02@gmail.com>
2023-09-14 14:59:23 +0330

    basic works for news app.

--- api/API/API/urls.py
+++ api/API/API/urls.py
@@ -9,6 +9,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include("accounts.urls")),
+    path('news/', include("news.urls")),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
